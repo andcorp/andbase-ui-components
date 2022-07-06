552af124abb64c472862b97e1287fd708372419d
a-yamaguchi <a-yamaguchi@andcorp.co.jp>
2022-07-06 19:16:29 +0900

    issue #13 ブランドロゴ追加

--- src/examples/Home.tsx
+++ src/examples/Home.tsx
@@ -2,6 +2,8 @@
  * Exampleサイト ホーム
  */
 
+import { PageHeader, PageHeaderItem } from "lib/components/PageHeader";
+import { Header } from "lib/layouts/Header";
 import { Page } from "lib/layouts/Page";
 import { Component } from "lib/types";
 
@@ -9,5 +11,18 @@ import { Component } from "lib/types";
  * Exampleサイト ホーム
  */
 export const Home: Component = () => {
-  return <Page>test</Page>;
+  return (
+    <>
+      <Header>
+        <PageHeader>
+          <PageHeaderItem>
+            <img src="/images/logo.png" alt="ANDCORP" width="128" height="44" />
+          </PageHeaderItem>
+          <PageHeaderItem grow>test</PageHeaderItem>
+          <PageHeaderItem>test2</PageHeaderItem>
+        </PageHeader>
+      </Header>
+      <Page>test</Page>
+    </>
+  );
 };
