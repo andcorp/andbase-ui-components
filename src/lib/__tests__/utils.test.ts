13f6f4c394e90be4c4e0ec1aadd474cb48845787
a-yamaguchi <a-yamaguchi@andcorp.co.jp>
2022-07-06 14:56:13 +0900

    issue #13 クラス名連結用関数追加

---
+++ src/lib/__tests__/utils.test.ts
@@ -0,0 +1,25 @@
+/**
+ * ユーティリティ関数のテスト
+ */
+
+import { joinClassNames } from "../utils";
+
+test("joinClassNamesのテスト", () => {
+  // 単一のクラス指定が正常に扱えること
+  expect(joinClassNames("test_class")).toStrictEqual("test_class");
+
+  // 複数のクラス指定が正常に扱えること
+  expect(joinClassNames("test_class1", "test_class2")).toStrictEqual(
+    "test_class1 test_class2"
+  );
+
+  // 途中の空文字列等は取り除かれること
+  expect(
+    joinClassNames("test_class1", "", null, undefined, "test_class2")
+  ).toStrictEqual("test_class1 test_class2");
+
+  // 空の入力の場合は空文字列が返ること。
+  expect(joinClassNames(undefined)).toStrictEqual("");
+  expect(joinClassNames(null)).toStrictEqual("");
+  expect(joinClassNames("")).toStrictEqual("");
+});
