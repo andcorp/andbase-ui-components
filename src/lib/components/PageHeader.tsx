091afc16a2eb08e49e0893442d9a48c81ff00b72
a-yamaguchi <a-yamaguchi@andcorp.co.jp>
2022-07-06 18:45:29 +0900

    issue #13 ページヘッダー項目追加

--- src/lib/components/PageHeader.tsx
+++ src/lib/components/PageHeader.tsx
@@ -26,3 +26,24 @@ export const PageHeader: ComponentWithChildren = ({
     </div>
   );
 };
+
+/**
+ * ページヘッダー項目要素
+ */
+export const PageHeaderItem: ComponentWithChildren = ({
+  className,
+  children,
+  testId,
+}) => {
+  // ヘッダークラス名
+  const headerClassName = React.useMemo(
+    () => joinClassNames("bl_pageHeader_item", className),
+    [className]
+  );
+
+  return (
+    <div className={headerClassName} data-testid={testId}>
+      {children}
+    </div>
+  );
+};
--- src/lib/components/__tests__/PageHeader.test.tsx
+++ src/lib/components/__tests__/PageHeader.test.tsx
@@ -4,7 +4,7 @@
 
 import { render } from "@testing-library/react";
 
-import { PageHeader } from "../PageHeader";
+import { PageHeader, PageHeaderItem } from "../PageHeader";
 
 describe("PageHeaderのテスト", () => {
   it("className指定が行えること", async () => {
@@ -23,6 +23,30 @@ describe("PageHeaderのテスト", () => {
       <PageHeader>
         <span data-testid="children">test</span>
       </PageHeader>
+    );
+
+    const elements = (await findAllByTestId("children"))[0];
+    expect(elements).toHaveTextContent("test");
+  });
+});
+
+describe("PageHeaderItemのテスト", () => {
+  it("className指定が行えること", async () => {
+    // レンダリング実行
+    const { findAllByTestId } = render(
+      <PageHeaderItem className="test_class" testId="header" />
+    );
+
+    const elements = (await findAllByTestId("header"))[0];
+    expect(elements).toHaveClass("bl_pageHeader_item", "test_class");
+  });
+
+  it("childrenが生成されていること", async () => {
+    // レンダリング実行
+    const { findAllByTestId } = render(
+      <PageHeaderItem>
+        <span data-testid="children">test</span>
+      </PageHeaderItem>
     );
 
     const elements = (await findAllByTestId("children"))[0];
